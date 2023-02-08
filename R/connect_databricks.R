ef42a527c6e8fe21c65598f0cffe57448d94b14b
Leandro Roser <learoser@gmail.com>
2023-02-08 06:16:09 -0300

    Update connect_databricks.R

--- R/connect_databricks.R
+++ R/connect_databricks.R
@@ -1,4 +1,23 @@
 
+#' Create a connection to databricks
+#' @param adress URL to the cluster
+#' @param port port
+#' @param organization organization id
+#' @param cluster cluster ID
+#' @param token access token
+#' @export
+
+databricks_jdbc <- function(address, port, organization, cluster, token) {
+    location <- Sys.getenv("DATABRICKS_JAR")
+    driver <- RJDBC::JDBC(driverClass = "com.databricks.client.jdbc.Driver",
+                          classPath = location)
+    con <- DBI::dbConnect(driver,
+                          sprintf("jdbc:databricks://%s:%s/default;transportMode=http;ssl=1;httpPath=sql/protocolv1/o/%s/%s;AuthMech=3;UID=token;PWD=%s",
+                                  address, port, organization, cluster, token))
+    con
+}
+
+
 #' Start cluster with ODBC
 #' @export
 
