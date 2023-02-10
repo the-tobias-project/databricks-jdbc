e35241cc467725a8ae1f76802ffbe53394c73195
leandroroser <learoser@gmail.com>
2023-02-09 21:48:55 -0300

    solve dynamic lybrary load

--- R/connect_databricks.R
+++ R/connect_databricks.R
@@ -3,12 +3,14 @@
 #' @param env_file env file folder
 #' @export
 
-connect_cluster <- function(env_file = ".env") {
+connect_cluster <- function(env_file = ".env", load_odbclibs = TRUE, odbc_module_path = Sys.getenv("HOME")) {
   dotenv::load_dot_env(env_file)
-  dyn.load(paste0(Sys.getenv("HOME"), "/odbc-module/driver/unixODBC-2.3.11/odbcinst/.libs/libodbcinst.so"),
-           now=TRUE)
-  dyn.load(paste0(Sys.getenv("HOME"), "/odbc-module/driver/unixODBC-2.3.11/DriverManager/.libs/libodbc.so.2"),
-                  now=TRUE)
+  if (load_odbclibs){
+    dyn.load(paste0(odbc_module_path, "/odbc-module/driver/unixODBC-2.3.11/odbcinst/.libs/libodbcinst.so"),
+             now=TRUE)
+    dyn.load(paste0(odbc_module_path, "/odbc-module/driver/unixODBC-2.3.11/DriverManager/.libs/libodbc.so.2"),
+             now=TRUE)
+  }
   con <- DBI::dbConnect(odbc::odbc(), "Databricks")
   con
 }
